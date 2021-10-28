f4cf54d37c0b002f9033c47ca5b96f1976d945ea
Eugene <72232749+EugeneIm@users.noreply.github.com>
2021-10-28 10:37:17 -0700

    Add files via upload
    all the node files download

---
+++ Node GitHub/NodeJS/NodeGitDayOne/01_artists_from_year.js
@@ -0,0 +1,46 @@
+
+function artistsFrom2020(artists) {
+  // Put your solution here
+}
+module.exports = artistsFrom2020
+
+const artists = [
+  {
+    song: "HOLIDAY",
+    name: "Lil Nas X",
+    year: 2020
+  },
+  {
+    song: "WAP",
+    name: "Cardi B",
+    year: 2020
+  },
+  {
+    song: "Say So",
+    name: "Doja Cat",
+    year: 2020
+  },
+  { 
+    song: "Old Town Road",
+    name: "Lil Nas X",
+    year: 2019
+  },
+  {
+    song: "Bad Guy",
+    name: "Billie Eilish",
+    year: 2019
+  },
+  {
+    song: "God's Plan",
+    name: "Drake",
+    year: 2018
+  }
+]
+
+console.log(artistsFrom2020(artists))
+// { 
+//   song: 'Bad Guy', 
+//   name: 'Billie Eilish', 
+//   year: 2019 
+// }
+
